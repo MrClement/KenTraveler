cdaa3330389813f151a3ade8a6aeea05c7fccf20
jpoley15 <jpoley15@10.80.9.64>
2013-09-19 09:51:58 -0600

    Added stuff to character

--- src/world/Character.java
+++ src/world/Character.java
@@ -2,4 +2,31 @@ package world;
 
 public class Character extends LivingThing {
 
+	private String name;
+	private int strength;
+	private int intelligence;
+	private int dexterity;
+	
+	// Constructs a default character with name "name" and stats all set to 10 //
+	
+	public Character(){
+		
+		name = "name";
+		strength = 10;
+		intelligence = 10;
+		dexterity = 10;
+		
+	}
+	
+	// Constructs a character with user specified name and stats //
+	
+	public Character(String name, int strength, int intelligence, int dexterity){
+		
+		this.name = name;
+		this.strength = strength;
+		this.intelligence = intelligence;
+		this.dexterity = dexterity;
+		
+	}
+	
 }

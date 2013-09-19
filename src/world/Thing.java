c1a29e46464684e2c04663980a83a695f0bb7fc1
King <hjames14@kentdenver.org>
2013-09-19 09:53:37 -0600

    Thing Updates

--- src/world/Thing.java
+++ src/world/Thing.java
@@ -2,4 +2,31 @@ package world;
 
 public abstract class Thing {
 
+	private boolean solid;
+	
+	/**
+	 * @param b and array of grid spaces
+	 */
+	public Thing(boolean b)
+	{
+		solid = b;
+	}
+	
+	/**
+	 * returns array of gridspaces
+	 */
+	public getPosition()
+	{
+		
+	}
+	
+	/**
+	 * @return whether the object is able to be walked through
+	 */
+	public boolean getSolid()
+	{
+		return solid;
+		
+	}
+	
 }

1a2e9602bfd0906d437f9ee7ff7576a0c0f6c84e
MrClement <aclement@10.80.32.112>
2013-09-19 09:48:02 -0600

    Added GridSpace

--- src/world/Grid.java
+++ src/world/Grid.java
@@ -1,5 +1,21 @@
 package world;
 
+import java.awt.Point;
+import java.util.HashMap;
+
+/**
+ * @author aclement
+ * 
+ */
 public class Grid {
+
+	private HashMap<Point, GridSpace> grid;
+
+	/**
+	 * 
+	 */
+	public Grid() {
+		grid = new HashMap<Point, GridSpace>();
+	}
 
 }

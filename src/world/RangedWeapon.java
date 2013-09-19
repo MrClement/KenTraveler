ad8ad80313941ce328ad2b5a81a7947dd0c5ffa1
cwilson14 <cwilson14@10.80.10.160>
2013-09-19 09:51:24 -0600

    Weapon and RangedWeapon

---
+++ src/world/RangedWeapon.java
@@ -0,0 +1,15 @@
+package world;
+
+public class RangedWeapon extends Weapon {
+	private int range;
+
+	public int getRange() {
+		return range;
+	}
+
+	public void setRange(int range) {
+		this.range = range;
+	}
+	
+	
+}
--- src/world/Weapon.java
+++ src/world/Weapon.java
@@ -1,7 +1,26 @@
 package world;
 
+/**
+ * @author cwilson14
+ * defines all weapons, including melee and ranged weapons
+ *
+ */
 public class Weapon extends Thing {
-	int damage;
-	int length;
+	private int damage;
+	private int length;
+	
+	public int getDamage() {
+		return damage;
+	}
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
 
+	
 }

7bb95293f349a59c089b8d25a29da500fd76c6e9
jpoley15 <jpoley15@10.80.9.103>
2013-09-23 08:11:12 -0600

    Added java doc stuff

--- src/world/Character.java
+++ src/world/Character.java
@@ -11,8 +11,10 @@ public class Character extends LivingThing {
 	private int xp;
 
 	// Constructs a default character with name "name" and stats all set to 10
-	// //
 
+	/**
+	 * @param solid this is a boolean indicating whether something is solid (true) or not (false)
+	 */
 	public Character(boolean solid) {
 
 		super(solid);
@@ -25,6 +27,13 @@ public class Character extends LivingThing {
 
 	// Constructs a character with user specified name and stats //
 
+	/**
+	 * @param name is a String that stores the name of the character
+	 * @param strength is an int that stores the character's strength
+	 * @param intelligence is an int that stores the character's intelligence
+	 * @param dexterity is an int that stores the character's dexterity 
+	 * @param solid is a boolean indicating whether the character is solid
+	 */
 	public Character(String name, int strength, int intelligence, int dexterity, boolean solid) {
 
 		super(solid);

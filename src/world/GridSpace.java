124e6df8a3e61d6d4d4c40efc88eda2f04fd4164
WesleyDencker <WesleyDencker@10.80.9.104>
2013-09-23 08:34:59 -0600

    Updated GridSpace

--- src/world/GridSpace.java
+++ src/world/GridSpace.java
@@ -1,5 +1,35 @@
 package world;
-
+import java.util.ArrayList;
+/**
+ * 
+ * @author WesleyDencker
+ *
+ */
 public class GridSpace {
+	//tick method to continue checking if things are in space
+	
+	private ArrayList<Thing> thingsInSpace;	
+	
+	public GridSpace(ArrayList<Thing> initialInGridSpace){
+		thingsInSpace = initialInGridSpace;
+	}
+	
+	private ArrayList<Thing> returnThings(){
+		return thingsInSpace;
+	}
+	
+	private Thing remove(Thing toRemove){
+		if (thingsInSpace.remove(toRemove)==true){
+			return toRemove;
+		}
+		else return null;
+	}
+	
+	private void onTick(){
+		
+	}
+	
+	
+	
 
 }

41bed4791adf1a2f372762f23fdd4cbce1b28023
WesleyDencker <WesleyDencker@10.80.9.104>
2013-09-24 15:02:15 -0600

    Added comments to GridSpace.

--- src/world/GridSpace.java
+++ src/world/GridSpace.java
@@ -9,13 +9,28 @@ public class GridSpace {
 	
 	private ArrayList<Thing> thingsInSpace;	
 	
+	/**
+	 * This is the GridSpace constructor, which constructs a GridSpace object with
+	 * an initial set of Thing objects.
+	 * @param initialInGridSpace is an ArrayList of Things that will initially be 
+	 * placed in the Grid.
+	 */
 	public GridSpace(ArrayList<Thing> initialInGridSpace){
 		thingsInSpace = initialInGridSpace;
 	}
 	
+	/**
+	 * This method returns all the Thing objects in the GridSpace.
+	 * @return an ArrayList of all the Thing objects in the GridSpace.
+	 */
 	public ArrayList<Thing> returnThings(){
 		return thingsInSpace;
 	}
+	
+	/**
+	 * This method returns all the LivingThing objects in the GridSpace.
+	 * @return an ArrayList of all the LivingThing objects in the GridSpace.
+	 */
 	public ArrayList<LivingThing> returnLivingThings(){
 		ArrayList<LivingThing> toReturn = new ArrayList<LivingThing>();
 		for (int i=0; i< thingsInSpace.size(); i++){
@@ -26,10 +41,19 @@ public class GridSpace {
 		return toReturn;
 	}
 	
+	/**
+	 * This method adds a Thing object to the GridSpace.
+	 * @param toAdd is the Thing object to be added to the GridSpace
+	 */
 	public void add(Thing toAdd){
 		thingsInSpace.add(toAdd);
 	}
 	
+	/**
+	 * This method removes the first instance of a Thing object from a GridSpace.
+	 * @param toRemove is a Thing object that needs to be removed.
+	 * @return the object that was removed.
+	 */
 	public Thing remove(Thing toRemove){
 		if (thingsInSpace.remove(toRemove)==true){
 			return toRemove;
@@ -38,14 +62,22 @@ public class GridSpace {
 	}
 	
 	
+	/**
+	 * This method applies damage to all LivingThing objects that are in the GridSpace with
+	 * a Weapon object, removes all LivingThings that have died, and moves anything that needs
+	 * to be moved into a different GridSpace.
+	 * @param toMove is an ArrayList of Thing objects that are to moved to a different
+	 * GridSpace, whose indices correspond with the destinations parameter.
+	 * @param destinations is an ArrayList of GridSpaces to which the Thing objects in the
+	 * toMove parameter will move.
+	 */
 	public void onTick(ArrayList<Thing> toMove, ArrayList<GridSpace> destinations){
 		//checks if there is a weapon modifier in the grid space, if so, applies damage
-		//to all living things within the space
 		for (int i = 0; i < thingsInSpace.size(); i++){
 			if (thingsInSpace.get(i) instanceof Weapon){
 				for (int j = 0; j < thingsInSpace.size(); j++){
 					if (thingsInSpace.get(j) instanceof LivingThing){
-						((Weapon) thingsInSpace.get(i)).doDamage(this);
+						((Weapon) thingsInSpace.get(i)).doDamage((LivingThing) thingsInSpace.get(j));
 					}
 				}
 			}

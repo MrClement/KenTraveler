1f33b0ce24db8dc284507aebc641a23dd0b8a9fa
jpoley15 <jpoley15@10.80.9.103>
2013-09-24 14:21:21 -0600

    Fixed character comments

--- src/world/Character.java
+++ src/world/Character.java
@@ -8,33 +8,13 @@ public class Character extends LivingThing {
 
 	private int xp;
 
-	// Constructs a default character with name "name" and stats all set to 10
-
-	/**
-	 * @param solid is a boolean indicating whether something is solid (true) or not (false)
-	 */
 	public Character(boolean solid) {
-
 		super(solid);
-
-
 	}
 
-	// Constructs a character with user specified name and stats //
-
-	/**
-	 * @param name is a String that stores the name of the character
-	 * @param strength is an int that stores the character's strength
-	 * @param intelligence is an int that stores the character's intelligence
-	 * @param dexterity is an int that stores the character's dexterity 
-	 * @param solid is a boolean indicating whether the character is solid
-	 */
 	public Character(String name, int strength, int intelligence, int dexterity, boolean solid) {
-
 		super(name, strength, intelligence, dexterity, solid);
-
 	}
-
 
 	public int getXp() {
 		return xp;

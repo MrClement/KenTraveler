df56cdaaa50f90e8de9af00c1030ecf59eff21a9
WesleyDencker <WesleyDencker@10.80.9.104>
2013-09-24 14:49:40 -0600

    Updated onTick method.

--- src/world/GridSpace.java
+++ src/world/GridSpace.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
  *
  */
 public class GridSpace {
-	//tick method to continue checking if things are in space
 	
 	private ArrayList<Thing> thingsInSpace;	
 	
@@ -27,6 +26,10 @@ public class GridSpace {
 		return toReturn;
 	}
 	
+	public void add(Thing toAdd){
+		thingsInSpace.add(toAdd);
+	}
+	
 	public Thing remove(Thing toRemove){
 		if (thingsInSpace.remove(toRemove)==true){
 			return toRemove;
@@ -34,8 +37,34 @@ public class GridSpace {
 		else return null;
 	}
 	
-	public void onTick(){
+	
+	public void onTick(ArrayList<Thing> toMove, ArrayList<GridSpace> destinations){
+		//checks if there is a weapon modifier in the grid space, if so, applies damage
+		//to all living things within the space
+		for (int i = 0; i < thingsInSpace.size(); i++){
+			if (thingsInSpace.get(i) instanceof Weapon){
+				for (int j = 0; j < thingsInSpace.size(); j++){
+					if (thingsInSpace.get(j) instanceof LivingThing){
+						((Weapon) thingsInSpace.get(i)).doDamage(this);
+					}
+				}
+			}
+		}
 		
+		//removes livingThings that have died
+		ArrayList<LivingThing> livingThings = returnLivingThings();
+		for (int k = 0; k < livingThings.size(); k++){
+			if (livingThings.get(k).getHp() <= 0){
+				while (remove(livingThings.get(k)) != null){
+					remove(livingThings.get(k));
+				}
+			}
+		}
+		
+		//anything that needs to move is moved
+		for (int l = 0; l < toMove.size(); l++){
+			destinations.get(l).add(remove(toMove.get(l)));
+		}
 	}
 	
 	

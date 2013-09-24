d3d28a0f082a6ac64a977fb2d6dfe64d1bc0c1e1
cwilson14 <cwilson14@10.80.10.160>
2013-09-24 15:03:42 -0600

    small changes to ranged weapons and magic

--- src/world/RangedWeapon.java
+++ src/world/RangedWeapon.java
@@ -5,9 +5,10 @@ public class RangedWeapon extends Weapon {
 	private int speed;
 	//ticks per cell
 	
-	public RangedWeapon(boolean b, LivingThing l, int r) {
+	public RangedWeapon(boolean b, LivingThing l, int r, int s) {
 		super(b,l);
 		range=r;
+		speed=s;
 		// TODO Auto-generated constructor stub
 	}
 
@@ -18,5 +19,14 @@ public class RangedWeapon extends Weapon {
 	public void setRange(int range) {
 		this.range = range;
 	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
 
 }

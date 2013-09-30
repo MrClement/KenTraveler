8de3f94fc54b804a7e8df4c4f8191d9e1a959a91
WesleyDencker <WesleyDencker@10.80.9.159>
2013-09-30 09:36:45 -0600

    Fixed driver

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -10,9 +10,9 @@ public class Driver {
 		GameDisplay display = new GameDisplay(g);
 		long s = System.currentTimeMillis();
 		while ((s - System.currentTimeMillis()) < 0){
-			s = System.currentTimeMillis();
 			if ((System.currentTimeMillis() - s) >= 1000){
 				display.redraw(g);
+				s = System.currentTimeMillis();
 			}
 		}
 	}

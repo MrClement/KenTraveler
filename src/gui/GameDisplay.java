4a6961b1ebd5402c82bc0a146aa1ed402aca1849
MrClement <aclement@10.80.32.112>
2013-09-30 09:27:07 -0600

    Added redraw

--- src/gui/GameDisplay.java
+++ src/gui/GameDisplay.java
@@ -8,6 +8,8 @@ import java.util.Random;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import world.Grid;
+
 public class GameDisplay {
 
 	private JFrame frame;
@@ -19,7 +21,7 @@ public class GameDisplay {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					GameDisplay window = new GameDisplay();
+					GameDisplay window = new GameDisplay(new Grid());
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -31,26 +33,34 @@ public class GameDisplay {
 	/**
 	 * Create the application.
 	 */
-	public GameDisplay() {
-		initialize();
+	public GameDisplay(Grid g) {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new GridLayout(25, 100, 0, 0));
+		frame.setLocationRelativeTo(null);
+		initialize(g);
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new GridLayout(25, 100, 0, 0));
+	private void initialize(Grid g) {
 
 		Random r = new Random();
-		for (int i = 0; i< 2500; i++){
+		for (int i = 0; i < 2500; i++) {
 			JPanel panel = new JPanel(true);
 			panel.setBackground(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
 			frame.add(panel);
 		}
 
+	}
+
+	public void redraw(Grid g) {
+		frame.getContentPane().removeAll();
+		initialize(g);
+		frame.getContentPane().validate();
+		frame.getContentPane().repaint();
 	}
 
 }

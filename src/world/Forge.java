a773470509c3b116676b3fb433d2ac7f08424495
cwilson14 <cwilson14@10.80.10.62>
2013-10-08 11:59:47 -0600

    fixed the Forge

--- src/world/Forge.java
+++ src/world/Forge.java
@@ -3,6 +3,8 @@
  */
 package world;
 
+import java.awt.Color;
+
 /**
  * @author cwilson14
  *	Constructs weapons
@@ -23,50 +25,50 @@ public class Forge {
 	
 	public Magic constructMagic(int i, LivingThing l){
 		if(i==0){
-			Magic fire1=new Magic(true, l, 5, 3, 1);
+			Magic fire1=new Magic(true, Color.ORANGE, l, 5, 3, 1);
 			Damage fire1Dam=new Damage(10, 0, 0, 0, 0, 0, 0, 0, false, false, false);
 			fire1.setDamage(fire1Dam);
 			return fire1;
 			
 		}
 		else if(i==1){
-			Magic fire2=new Magic(true, l, 10, 8, 1);
+			Magic fire2=new Magic(true, Color.ORANGE, l, 10, 8, 1);
 			Damage fire2Dam=new Damage(20, 0, 0, 0, 0, 0, 0, 0, false, false, false);
 			fire2.setDamage(fire2Dam);
 			return fire2;
 		}
 		else if(i==2){
-			Magic fire2=new Magic(true, l, 20, 15, 1);
+			Magic fire2=new Magic(true, Color.ORANGE, l, 20, 15, 1);
 			Damage fire2Dam=new Damage(20, 0, 0, 0, 0, 0, 0, 7, false, false, false);
 			fire2.setDamage(fire2Dam);
 			return fire2;
 		}
 		else if(i==3){
-			Magic ice1=new Magic(true, l, 5, 3, 1);
+			Magic ice1=new Magic(true, new Color(200, 249, 250), l, 5, 3, 1);
 			Damage ice1Dam=new Damage(7, 0, 0, 0, 5, 0, 0, 0, false, false, false);
 			ice1.setDamage(ice1Dam);
 			return ice1;
 		}
 		else if(i==4){
-			Magic ice2=new Magic(true, l, 10, 8, 1);
+			Magic ice2=new Magic(true, new Color(200, 249, 250), l, 10, 8, 1);
 			Damage ice2Dam=new Damage(15, 0, 0, 0, 10, 0, 0, 0, false, false, false);
 			ice2.setDamage(ice2Dam);
 			return ice2;
 		}
 		else if(i==5){
-			Magic ice3=new Magic(true, l, 20, 15, 1);
+			Magic ice3=new Magic(true, new Color(200, 249, 250), l, 20, 15, 1);
 			Damage ice3Dam=new Damage(15, 0, 0, 0, 10, 0, 0, 0, false, false, true);
 			ice3.setDamage(ice3Dam);
 			return ice3;
 		}
 		else if(i==6){
-			Magic solidify=new Magic(true, l, 8, 11, 1);
+			Magic solidify=new Magic(true, Color.DARK_GRAY, l, 8, 11, 1);
 			Damage solidifyDamage=new Damage(0, 0, 0, 0, 0, 0, 0, 0, true, false, false);
 			solidify.setDamage(solidifyDamage);
 			return solidify;
 		}
 		else if(i==7){
-			Magic gassify=new Magic(true, l, 8, 11, 1);
+			Magic gassify=new Magic(true, Color.LIGHT_GRAY, l, 8, 11, 1);
 			Damage gassifyDamage=new Damage(0, 0, 0, 0, 0, 0, 0, 0, true, false, false);
 			gassify.setDamage(gassifyDamage);
 			return gassify;
@@ -81,37 +83,37 @@ public class Forge {
 	//5=throwing knives
 	public RangedWeapon constructRangedWeapons(int i, LivingThing l){
 		if(i==0){
-			RangedWeapon baseBow=new RangedWeapon(true, l, 15, 1);
+			RangedWeapon baseBow=new RangedWeapon(true, Color.BLUE, l, 15, 1);
 			Damage baseBowDamage=new Damage(7, 0, 0, 0, 0, 0, 0, 0, false, false, false);
 			baseBow.setDamage(baseBowDamage);
 			return baseBow;
 		}
 		else if(i==1){
-			RangedWeapon longBow=new RangedWeapon(true, l, 30, 1);
+			RangedWeapon longBow=new RangedWeapon(true, Color.BLUE, l, 30, 1);
 			Damage longBowDamage=new Damage(15, 0, 0, 0, 0, 0, 0, 0, false, false, false);
 			longBow.setDamage(longBowDamage);
 			return longBow;
 		}
 		else if(i==2){
-			RangedWeapon crossBow=new RangedWeapon(true, l, 20, 1);
+			RangedWeapon crossBow=new RangedWeapon(true, Color.BLUE, l, 20, 1);
 			Damage crossBowDamage=new Damage(15, 0, 0, 0, 0, 0, 0, 0, false, false, false);
 			crossBow.setDamage(crossBowDamage);
 			return crossBow;
 		}
 		else if(i==3){
-			RangedWeapon crossBowPlus=new RangedWeapon(true, l, 30, 1);
+			RangedWeapon crossBowPlus=new RangedWeapon(true, Color.BLUE, l, 30, 1);
 			Damage crossBowPlusDamage=new Damage(25, 0, 0, 0, 0, 0, 0, 0, false, false, false);
 			crossBowPlus.setDamage(crossBowPlusDamage);
 			return crossBowPlus;
 		}
 		else if(i==4){
-			RangedWeapon javelin=new RangedWeapon(true, l, 10, 1);
+			RangedWeapon javelin=new RangedWeapon(true, Color.BLUE, l, 10, 1);
 			Damage javelinDamage=new Damage(20, 0, 0, 0, 0, 0, 0, 0, false, false, false);
 			javelin.setDamage(javelinDamage);
 			return javelin;
 		}
 		else if(i==5){
-			RangedWeapon throwingKnives=new RangedWeapon(true, l, 15, 1);
+			RangedWeapon throwingKnives=new RangedWeapon(true, Color.BLUE, l, 15, 1);
 			Damage throwingKnivesDamage=new Damage(10, 0, 0, 0, 0, 0, 0, 0, false, false, false);
 			throwingKnives.setDamage(throwingKnivesDamage);
 			return throwingKnives;

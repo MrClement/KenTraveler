daab91f1beef0451bd6fdf23b4dd0730c7de8ff5
MrClement <aclement@10.80.32.173>
2013-11-13 11:32:34 -0700

    Fixed loop update

--- src/world/World.java
+++ src/world/World.java
@@ -49,13 +49,13 @@ public class World {
 				} else {
 					f.add(new Terrain(true, a.getTopColor()));
 				}
-				if (a.getHill()){
+				if (a.getHill()) {
 					this.drawHill(g);
 				}
 
-					// needs to check if there is a hill in the background then
-					// call
-					// the draw hill method
+				// needs to check if there is a hill in the background then
+				// call
+				// the draw hill method
 				f.sortArrayOfThings();
 				grid.put(new Point(d, j), f);
 			}
@@ -76,31 +76,29 @@ public class World {
 		int seed;
 		height = r.nextInt(20) + 1;
 		seed = r.nextInt(50) + 2;
-		
-		HashMap<Point, GridSpace> grid = g.getGrid();
-		ArrayList<Integer> a = new ArrayList <Integer>();
-		int u = -1;
-		
-			for(int p = 0; p <= height; p = p+3)
-			{
-				a.add(23-p);
-			}
-			System.out.println(a);
-			int xpos = 0;
-		for(int i = 0; i < a.size(); i++){
-			width = (r.nextInt(7) + 1) + seed;
-			for(int s = 0; s < width; s++){
-			int topBlock = a.get(i);
-			for(int j = 23; j > topBlock; j--)
-			{
-				GridSpace f = new GridSpace(new ArrayList<Thing>());
-				f.add(new Terrain(true, Color.GREEN));
-				grid.put(new Point(xpos++, j), f);
-				
-			}	
-		}
-		}
 
+		HashMap<Point, GridSpace> grid = g.getGrid();
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		int u = -1;
+
+		for (int p = 0; p <= height; p = p + 3) {
+			a.add(23 - p);
+		}
+		System.out.println(a);
+		int xpos = 0;
+		for (int i = 0; i < a.size(); i++) {
+			width = (r.nextInt(7) + 1) + seed;
+			for (int s = 0; s < width; s++) {
+				int topBlock = a.get(i);
+				for (int j = 23; j > topBlock; j--) {
+					GridSpace f = new GridSpace(new ArrayList<Thing>());
+					f.add(new Terrain(true, Color.GREEN));
+					grid.put(new Point(xpos, j), f);
+
+				}
+				xpos++;
+			}
+		}
 
 	}
 

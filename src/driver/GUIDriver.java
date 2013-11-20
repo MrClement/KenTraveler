9c46f3f6079bb26f03caae7ee08229bb1bb0259c
MrClement <aclement@10.80.32.173>
2013-11-20 13:44:56 -0700

    Caught weird error

--- src/driver/GUIDriver.java
+++ src/driver/GUIDriver.java
@@ -218,12 +218,26 @@ public class GUIDriver {
 				g2d.drawString(String.format("FPS: %s", fps), 20, 20);
 				g2d.drawString(String.format("Stage: %s", stage), 100, 20);
 				g2d.drawString(String.format("Enemies killed: %s", g.getNumKilled()), 180, 20);
-				switch(g.getGrid().get(g.getCharacterLocation()).returnCharacter().getHp()){
-				case 20: g2d.drawString("Health: * * * *", 320, 20); break;
-				case 15: g2d.drawString("Health: * * * _", 320, 20); break;
-				case 10: g2d.drawString("Health: * * _ _", 320, 20); break;
-				case 5: g2d.drawString("Health: * _ _ _", 320, 20); break;
-				default: g2d.drawString("Health: _ _ _ _", 320, 20); break;
+				try {
+					switch (g.getGrid().get(g.getCharacterLocation()).returnCharacter().getHp()) {
+						case 20:
+							g2d.drawString("Health: * * * *", 320, 20);
+							break;
+						case 15:
+							g2d.drawString("Health: * * * _", 320, 20);
+							break;
+						case 10:
+							g2d.drawString("Health: * * _ _", 320, 20);
+							break;
+						case 5:
+							g2d.drawString("Health: * _ _ _", 320, 20);
+							break;
+						default:
+							g2d.drawString("Health: _ _ _ _", 320, 20);
+							break;
+					}
+				} catch (NullPointerException e) {
+					System.out.println("Caught that error");
 				}
 
 				// Blit image and flip...

aebcd57cf44ff25edc3f50ed3619ab7673e86b19
cwilson14 <cwilson14@10.80.10.190>
2013-12-18 10:38:32 -0700

    better merchant

--- src/world/Merchant.java
+++ src/world/Merchant.java
@@ -10,17 +10,20 @@ public class Merchant {
 		Magic m=f.constructMagic(i, c);
 		c.addWeapon(m);
 		c.addMoney(-20);
+		System.out.println("You just bought a:" + m.toString());
 	}
 	
 	public void buyMeleeWeapons(int i, Character c){
-		Weapon w=f.constructMeleeWeapons(i, 5, c);
+		Weapon w=f.constructMeleeWeapons(i, c);
 		c.addWeapon(w);
 		c.addMoney(-20);
+		System.out.println("You just bought a:" + w.toString());
 	}
 	
 	public void buyRangedWeapons(int i, Character c){
 		RangedWeapon r=f.constructRangedWeapons(i, c);
 		c.addWeapon(r);
 		c.addMoney(-20);
+		System.out.println("You just bought a:" + r.toString());
 	}
 }

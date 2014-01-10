92cb4d90f8ca641209e4d7cc27ff4f532493a77b
MrClement <aclement@10.80.32.79>
2014-01-10 11:28:41 -0700

    Basic output

--- src/driver/SaveGameWriter.java
+++ src/driver/SaveGameWriter.java
@@ -1,32 +1,37 @@
 package driver;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
 import world.Character;
 
 public class SaveGameWriter {
-	
-	
-	public SaveGameWriter(){
-		
+
+	public SaveGameWriter() {
+
 	}
-	
-	public void writeSaveGame(Character c){
-		int gold=c.getMoney();
-		int level=c.getLevel();
-		int exp=c.getXp();
-		int hp=c.getHp();
-		int ammo=c.getAmmo();
-		int mana=c.getMana();
-		int maxHP=c.getMaxHp();
-		int str=c.getStrength();
-		int intelligence=c.getIntelligence();
-		int dex=c.getDexterity();
-		int Speed=c.getDexterity();
-		String name=c.getName();
-		
-		
+
+	public void writeSaveGame(Character c) {
+		int gold = c.getMoney();
+		int level = c.getLevel();
+		int exp = c.getXp();
+		int hp = c.getHp();
+		int ammo = c.getAmmo();
+		int mana = c.getMana();
+		int maxHP = c.getMaxHp();
+		int str = c.getStrength();
+		int intelligence = c.getIntelligence();
+		int dex = c.getDexterity();
+		int Speed = c.getDexterity();
+		String name = c.getName();
+
+		File f = new File("save.txt");
+		BufferedWriter out = new BufferedWriter(new FileWriter(f));
+
 	}
-	
-	public void loadSaveGame(){
-	
+
+	public void loadSaveGame() {
+
 	}
 }
